1e9551a80ecb09af813a6e37cf707628c386f2e2
KashifSidd <kashifsiddiqui.it@gmail.com>
2024-02-13 22:55:26 +0530

    refactor: improvement

--- src/components/profileImage.js
+++ src/components/profileImage.js
@@ -1,18 +1,23 @@
 import React from "react";
 
-const ProfileImage = ({ name , available}) => {
+// ProfileImage component takes two props: name and available
+const ProfileImage = ({ name, available }) => {
+  // Split the name into parts to extract initials
   const nameParts = name.split(" ");
   const firstNameInitial = nameParts[0] ? nameParts[0][0] : "";
   const lastNameInitial = nameParts[1] ? nameParts[1][0] : "";
 
+  // Render the user profile image with initials
   return (
     <div>
-        <span style={{color:'#ffffff'}} className="user-profile-image">
-            {firstNameInitial}
-            {lastNameInitial}
-        </span>
-        {available && (<span className="available" style={{fontSize : '2rem' , color: '#ffc400'}}>•</span>)}
-        
+      {/* Display the user profile image with initials */}
+      <span style={{ color: '#ffffff' }} className="user-profile-image">
+        {firstNameInitial}
+        {lastNameInitial}
+      </span>
+
+      {/* Display an available indicator if available is true */}
+      {available && (<span className="available" style={{ fontSize: '2rem', color: '#ffc400' }}>•</span>)}
     </div>
   );
 };
